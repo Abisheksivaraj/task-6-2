062df17101867ccd70181dc337b86231406da129
abisheksivaraj2001@gmail.com <abisheksivaraj2001@gmail.com>
2023-08-28 13:50:13 +0530

    balance

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use script";
 
-const formEel = document.getElementById("form")
+const formEle = document.getElementById("form")
 
 const nameEl = document.getElementById("username")
 
@@ -10,33 +10,55 @@ const passwordEl = document.getElementById("password")
 
 const checkEl = document.getElementById("check")
 
+// error validation
+const nameInputForm = document.getElementById('username-input-field')
 
-function checkRequired(inputs){
-  inputs.forEach((input) => {
-    if(input.value.trim() === "") {
-      errorInput(input, "enter the required text")
-    }else{
-      successInput(input);
+const emailInputForm = document.getElementById('email-input-field')
+
+const passwordInputForm = document.getElementById('password-input-field')
+
+const confirmPasswordInputForm = document.getElementById('confirm-pass-input-field');
+
+// error msg
+const emailErrMsg = document.getElementById('email-error');
+
+const passwordErrorMsg = document.getElementById('password-error');
+
+const confirmPasswordErrorMsg = document.getElementById('check-password-error');
+
+// gv
+const pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+
+// function
+const checkValidation = ()=>{
+  // username validation
+  if(nameEl.value){
+    if(nameInputForm.classList.contains('fail')){
+      nameInputForm.classList.remove('fail')
     }
-  });
+    nameInputForm.classList.add('success')
+  }else if(nameEl.value == ""){
+    if(nameInputForm.classList.contains('success')){
+      nameInputForm.classList.remove('success')
+    }
+    nameInputForm.classList.add('fail')
+  }
+  // email validation
+  if(mailEl.value){
+    if(!(mailEl.value.match(pattern))){
+      emailErrMsg.classList.add('fail');
+      emailErrMsg.innerHTML = `Enter a Valid Email!`;
+    }else{
+      emailInputForm.classList.contains('fail') ? emailInputForm.classList.remove('fail') : emailInputForm.classList.add('success');
+    }
+  }else if(mailEl.value == ''){
+    emailErrMsg.classList.add('fail');
+    emailErrMsg.innerHTML = `Email cannot be empty`;
+  }
 }
 
-function errorInput(input, message){}
-
-function successInput(input) {
-  const inputForm = input.parentElement;
-
-  inputForm.className = "input-form success";
-
-  const i = inputForm.querySelector("i");
-
-  i.innerHTML = "";
-}
-
-
-Form.addEventListner("submit", function(e){
-
-  e.preventDefault();
-  checkRequired([nameEl, mailEl, passwordEl, checkEl]);
-
+// event listeners
+formEle.addEventListener("submit",(event)=>{
+  event.preventDefault();
+  checkValidation();
 })
